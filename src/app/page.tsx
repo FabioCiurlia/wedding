051da89f8e65570b8452b028a65fe55207f8ce16
Fabio <fabio.ciurlia@live.it>
2023-10-21 20:29:17 +0200

    colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import '../../node_modules/spectre.css/dist/spectre.css'
 
 export default function Home() {
   return (
-    <div className='container'>
-      <div className='columns'>
-        <div className='column col-6' style={{background: 'blue'}}>Six</div>
-        <div className='column col-3' style={{background: 'yellow'}}>Three</div>
-        <div className='column col-3' style={{background: 'green'}}>Three</div>
+    <div className='hero hero-lg'>
+      <div className='hero-body'>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column col-6' style={{ background: '#fdf7f5' }}>Six</div>
+            <div className='column col-3' style={{ background: '#f8d8cd' }}>Three</div>
+            <div className='column col-3' style={{ background: '#d1ae5b' }}>Three</div>
+          </div>
+        </div>
       </div>
     </div>
+
   )
 }
