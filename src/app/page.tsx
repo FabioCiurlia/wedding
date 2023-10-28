e26884395b046ca30d30e73d51efb27defc60e40
Fabio Ciurlia <fabio.ciurlia@live.it>
2023-10-28 22:25:59 +0200

    Added fonts and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,20 @@ import '../../node_modules/spectre.css/dist/spectre.css'
 
 export default function Home() {
   return (
-    <div className='hero paper-back' style={{ height: '100vh' }}>
+    <div className='container paper-back'>
+      <div className='hero' style={{ height: '100vh' }}>
       <div className='hero-body'>
-        <div className='container m-auto'>
-          <h1 style={{ fontFamily: 'august-script', fontSize: '32px'}}>Andrea e Roberta</h1>
+        <div className='container'>
+          <h1 style={{ fontFamily: 'august-script', fontSize: '72px', color: 'var(--light-green)', textAlign: 'center'}}>Andrea e Roberta</h1>
+          <div>
+            <p style={{ textAlign: 'center' }}>ANNUNCIANO CON GIOIA<br/>
+            IL LORO MATRIMONIO</p>
+          </div>
         </div>
       </div>
     </div>
+    </div>
+    
 
   )
 }
