9950cbb746cad4a40028c5367b26da9131df74f2
Fabio Ciurlia <fabio.ciurlia@live.it>
2023-10-28 16:32:20 +0200

    Added August font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,10 @@ import '../../node_modules/spectre.css/dist/spectre.css'
 
 export default function Home() {
   return (
-    <div className='hero hero-lg'>
+    <div className='hero paper-back' style={{ height: '100vh' }}>
       <div className='hero-body'>
-        <div className='container'>
-          <div className='columns'>
-            <div className='column col-6' style={{ background: '#fdf7f5' }}>Six</div>
-            <div className='column col-3' style={{ background: '#f8d8cd' }}>Three</div>
-            <div className='column col-3' style={{ background: '#d1ae5b' }}>Three</div>
-          </div>
+        <div className='container m-auto'>
+          <h1 style={{ fontFamily: 'august-script', fontSize: '32px'}}>Andrea e Roberta</h1>
         </div>
       </div>
     </div>
