4a3925a2ce2963a0e3c9495606b5763e74b786f6
Fabio Ciurlia <fabio.ciurlia@live.it>
2023-11-03 23:20:07 +0100

    Improve form

--- src/app/components/ConfirmForm.tsx
+++ src/app/components/ConfirmForm.tsx
@@ -1,33 +1,33 @@
+import React from "react";
+
 export default function ConfirmForm() {
   return (
     <>
-      <form name="confirm-invite" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
+      <form name="confirm-invite" data-netlify="true" method="post" onSubmit="submit">
+        <div className="field">
+          <label className="label">Nome</label>
+          <input className="input" type="text" name="name" placeholder="Nome"/>
+        </div>
+        <div className="field">
+            <label className="label">Cognome</label>
+            <input className="input" type="text" name="cognome" placeholder="Nome"/>
+        </div>
+        <div className="control">
+            <label className="radio">Parteciperai?</label>
+            <label className="radio">
+                <input type="radio" name="partecipi" value="Si"/>Si
+            </label>
+            <label className="radio">
+                <input type="radio" name="partecipi" value="No"/>No
+            </label>
+        </div>
+        <br/>
+        <div className="field">
+          <label className="label">Partecipanti</label>
+          <input className="input" type="number" name="numero"/>
+        </div>
+        <p className="has-text-centered">
+          <button className="button" type="submit">Send</button>
         </p>
       </form>
     </>
