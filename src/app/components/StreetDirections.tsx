52357ce7d0b93901d1195596d2f3aed8d4f57ff1
Fabio Ciurlia <fabio.ciurlia@live.it>
2023-11-08 00:00:24 +0100

    update apostrofo

--- src/app/components/StreetDirections.tsx
+++ src/app/components/StreetDirections.tsx
@@ -4,8 +4,8 @@ export default function StreetDirections() {
             <div className="has-text-centered">
                 <p className="wrapped-border is-size-5 mb-5">INDICAZIONI STRADALI E PARCHEGGI</p>
                 <div className="my-2 content has-text-justified">
-                    <p>Piazza del Duomo è collocata nel cuore del centro storico di Lecce ed è una Zona a Traffico Limitato (ZTL). E' uno dei singolari esempi di <i>Piazza Chiusa</i>, cioè completamente chiusa da tre lati e dispone soltanto di un ingresso laddove Via Giuseppe Libertini incontra Via Vittorio Emanuele II.</p>
-                    <p>Posto nelle immediate vicinanze della ZTL, in corrispondenza di Porta Napoli, il parcheggio del piazzale di via Adua costituisce un'ottima soluzione per raggiungere comodamente a piedi la Cattedrale. <br/> In alternativa, vicino al parcheggio di <span className="is-gold"><a>via Adua</a></span>, si consiglia l'area di parcheggio di <span className="is-gold"><a>Viale dell’Universita</a></span>.</p>
+                    <p>Piazza del Duomo è collocata nel cuore del centro storico di Lecce ed è una Zona a Traffico Limitato (ZTL). E&apos; uno dei singolari esempi di <i>Piazza Chiusa</i>, cioè completamente chiusa da tre lati e dispone soltanto di un ingresso laddove Via Giuseppe Libertini incontra Via Vittorio Emanuele II.</p>
+                    <p>Posto nelle immediate vicinanze della ZTL, in corrispondenza di Porta Napoli, il parcheggio del piazzale di via Adua costituisce un&apos;ottima soluzione per raggiungere comodamente a piedi la Cattedrale. <br/> In alternativa, vicino al parcheggio di <span className="is-gold"><a>via Adua</a></span>, si consiglia l&apos;area di parcheggio di <span className="is-gold"><a>Viale dell’Università</a></span>.</p>
                 </div>
                 
             </div>
