bc5c2383fc01e2d3acf6ec05c7deb814ccfc362a
Fabio Ciurlia <fabio.ciurlia@live.it>
2023-11-09 16:10:23 +0100

    Update viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Andrea e Roberta - 16 Dicembre Sposi',
   description: 'Tutte le informazioni sul nostro matrimonio',
+  viewport: 'user-scalable=no, width=device-width, initial-scale=1.0',
+  appleWebApp: true
 }
 
 export default function RootLayout({
